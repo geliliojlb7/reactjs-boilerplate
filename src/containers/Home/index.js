2534b73db4e2815e4a0786fbbd318b8d5b5c979a
geliliojlb7 <geliliojohnlloydb7@gmail.com>
2021-02-11 18:09:36 +0800

    edit modal

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     Button,
     Typography,
@@ -7,6 +7,7 @@ import {
     Radio,
     Select,
     DatePicker,
+    Modal,
 } from "antd";
 import Checkbox from "antd/lib/checkbox/Checkbox";
 
@@ -45,69 +46,98 @@ const columns = [
         key: "address",
     },
 ];
-const Home = () => (
-    <div>
-        <Button type="dashed" shape="round">
-            Dashed
-        </Button>
-        <Button type="dashed" danger shape="round">
-            Dashed
-        </Button>
-        <Button type="default" shape="round">
-            Default
-        </Button>
-        <Button type="default" danger shape="round">
-            Default
-        </Button>
-        <Button type="ghost" shape="round">
-            Ghost
-        </Button>
-        <Button type="ghost" danger shape="round">
-            Ghost
-        </Button>
-        <Button type="link" shape="round">
-            Link
-        </Button>
-        <Button type="link" danger shape="round">
-            Link
-        </Button>
-        <Button type="primary" shape="round">
-            Primary
-        </Button>
-        <Button type="primary" danger shape="round">
-            Danger
-        </Button>
-        <Button type="text" shape="round">
-            Text
-        </Button>
-        <Button type="text" danger shape="round">
-            Text
-        </Button>
-        <br />
-        <br />
-        <Title>h1. Ant Design</Title>
-        <Title level={2}>h2. Ant Design</Title>
-        <Title level={3}>h3. Ant Design</Title>
-        <Title level={4}>h4. Ant Design</Title>
-        <Title level={5}>h5. Ant Design</Title>
-        <Checkbox>Test</Checkbox>
-        <Checkbox disabled>Test</Checkbox>
-        <Radio>Test</Radio>
-        <Radio disabled>Test</Radio>
-        <Radio disabled checked>
-            Test
-        </Radio>
-        <Input placeholder="test" />
-        <Input placeholder="test" disabled />
-        <Select style={{ width: "250px" }}>
-            <Option>Test1</Option>
-            <Option disabled>Test2</Option>
-            <Option>Test3</Option>
-        </Select>
-        <DatePicker showTime />
-        <RangePicker showTime={{ format: "HH:mm" }} format="YYYY-MM-DD HH:mm" />
-        <Table dataSource={dataSource} columns={columns} size="small" />
-    </div>
-);
+
+const Home = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+    return (
+        <div>
+            <Button type="dashed" shape="round">
+                Dashed
+            </Button>
+            <Button type="dashed" danger shape="round">
+                Dashed
+            </Button>
+            <Button type="default" shape="round">
+                Default
+            </Button>
+            <Button type="default" danger shape="round">
+                Default
+            </Button>
+            <Button type="ghost" shape="round">
+                Ghost
+            </Button>
+            <Button type="ghost" danger shape="round">
+                Ghost
+            </Button>
+            <Button type="link" shape="round">
+                Link
+            </Button>
+            <Button type="link" danger shape="round">
+                Link
+            </Button>
+            <Button type="primary" shape="round" onClick={showModal}>
+                Primary
+            </Button>
+            <Button type="primary" danger shape="round">
+                Danger
+            </Button>
+            <Button type="text" shape="round">
+                Text
+            </Button>
+            <Button type="text" danger shape="round">
+                Text
+            </Button>
+            <br />
+            <br />
+            <Title>h1. Ant Design</Title>
+            <Title level={2}>h2. Ant Design</Title>
+            <Title level={3}>h3. Ant Design</Title>
+            <Title level={4}>h4. Ant Design</Title>
+            <Title level={5}>h5. Ant Design</Title>
+            <Checkbox>Test</Checkbox>
+            <Checkbox disabled>Test</Checkbox>
+            <Radio>Test</Radio>
+            <Radio disabled>Test</Radio>
+            <Radio disabled checked>
+                Test
+            </Radio>
+            <Input placeholder="test" />
+            <Input placeholder="test" disabled />
+            <Select style={{ width: "250px" }}>
+                <Option>Test1</Option>
+                <Option disabled>Test2</Option>
+                <Option>Test3</Option>
+            </Select>
+            <DatePicker showTime />
+            <RangePicker
+                showTime={{ format: "HH:mm" }}
+                format="YYYY-MM-DD HH:mm"
+            />
+            <Table dataSource={dataSource} columns={columns} size="small" />
+            <Modal
+                title="Basic Modal"
+                visible={isModalVisible}
+                onOk={handleOk}
+                onCancel={handleCancel}
+            >
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
+        </div>
+    );
+};
 
 export default Home;
